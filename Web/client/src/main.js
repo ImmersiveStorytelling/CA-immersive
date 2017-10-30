5a5e1cecbd929355473173f93a665541d4be728a
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2017-10-30 23:12:38 +0100

    app module veilig verwijderd

--- Web/client/src/main.js
+++ Web/client/src/main.js
@@ -1,9 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-// app module
-var app_module_1 = require("./app/app.module");
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 //login module
 var login_module_1 = require("./app/login/login.module");
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(login_module_1.LoginModule);
--- Web/client/src/main.ts
+++ Web/client/src/main.ts
@@ -1,9 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-// app module
-import { AppModule } from './app/app.module';
-platformBrowserDynamic().bootstrapModule(AppModule);
-
 //login module
 import { LoginModule } from './app/login/login.module';
 platformBrowserDynamic().bootstrapModule(LoginModule);
