32143f706a3962c893a554a7f9ad679c7538c9e2
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2017-10-31 21:19:45 +0100

    DataService aangemaakt, httpClient werkt nog niet

---
+++ Web/client/src/app/Service/DataService.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+@Injectable()
+export class DataService {
+
+  url = 'http://localhost:4000';
+
+  constructor(private http: HttpClientModule) {}
+
+  getData() {
+    return 'DATAA';
+    /*this.http.get<User>(this.url).subscribe(res => {
+      // this.users = res;
+      console.log(res);
+    });*/
+  }
+
+}
+
+interface User {
+  userID: string;
+  password: string;
+}
